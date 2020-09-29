678524331d80fb1eb72715e052b3761d9d592bd4
attiliogiglio <attilio.g93@gmail.com>
2020-09-29 14:26:51 -0300

    password generator

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
 import random
+from string import ascii_letters
 
+def password_generator():
 
+    upper_case = ['A', 'B', 'C', 'D', 'E']
+    lower_case = ['a', 'b', 'c', 'd', 'e']
+    simbols = ['!', '#', '$', '&', '?']
+    numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9' '0']
 
+    character = upper_case + lower_case + simbols + numbers
+
+    password = []    
+
+    for i in range(15) :
+        character_random = random.choice(character)
+        password.append(character_random)
+
+    password = ''.join(password)
+    
+    return password
 
 def run():
     password = password_generator()
